ccf6c956d02b1a8c90d5abb1e404ee139199cb6b
carlo1404 <brunoreyes150@gmail.com>
2025-04-06 09:08:42 -0500

    estos son estilos

--- php/include/footer.php
+++ php/include/footer.php
@@ -1,17 +1,17 @@
 <footer class="footer">
     <div class="footer__item">
-        <img src="img/icono-volante.png" alt="Volante" class="footer__icon">
+        <img src="img/steering-wheel-svgrepo-com.svg" alt="Volante" class="footer__icon">
         <p class="footer__text">TEST DRIVE</p>
         <hr class="footer__line">
     </div>
 
     <div class="footer__item footer__center">
-        <p class="footer__text footer__dealer-name">NOMBRE__CONCESIONARIO</p>
-        <img src="img/icono-auto.png" alt="Auto" class="footer__icon footer__car-icon">
+        <h3 class="footer__text footer__dealer-name">Solar Cars S.A.</h3>
+        <img src="img/logo.png" alt="Auto" class="footer__icon footer__car-icon">
     </div>
 
     <div class="footer__item">
-        <img src="img/icono-telefono.png" alt="Teléfono" class="footer__icon">
+        <img src="img/phone-svgrepo-com.svg" alt="Teléfono" class="footer__icon">
         <p class="footer__text">CONTACTANOS</p>
         <hr class="footer__line">
     </div>
