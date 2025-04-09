386912813653a312d6a60bfb5d608ebeb89f0546
stefanny834 <skaylopez2904@gmail.com>
2025-04-08 21:41:49 -0500

    cambie el footer por un include para reutilizar codigo

---
+++ php/include/login-footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <div class="footer-content">
+      <div class="footer-section about">
+        <h3>Sobre Nosotros</h3>
+        <p>SolarAuto es tu concesionario de confianza para compra de autos 100% seguros.</p>
+      </div>
+      <div class="footer-section contact">
+        <h3>Contacto</h3>
+        <p>📞 +57 123 456 7890</p>
+        <p>📧 contacto@solarauto.com</p>
+      </div>
+      <div class="footer-section location">
+        <h3>Ubicación</h3>
+        <p>🚗 Av. Automotriz #123, PEREIRA</p>
+      </div>
+      <div class="footer-section social-icons">
+        <h3>Síguenos</h3>
+        <img src="../img/facebook.png" alt="Facebook">
+        <img src="../img/instagram.gif" alt="Instagram">
+        <img src="../img/twitter.png" alt="Twitter">
+      </div>
+    </div>
+   
+  </footer>
--- php/login.php
+++ php/login.php
@@ -6,7 +6,6 @@
     <title>Inicio de sesión</title>
     <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="css/Normalize.css">
-    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="body">
     <header class="header">
@@ -30,30 +29,7 @@
 
         </form>
     </main>
-    <footer class="footer">
-    <div class="footer-content">
-      <div class="footer-section about">
-        <h3>Sobre Nosotros</h3>
-        <p>SolarAuto es tu concesionario de confianza para compra de autos 100% seguros.</p>
-      </div>
-      <div class="footer-section contact">
-        <h3>Contacto</h3>
-        <p>📞 +57 123 456 7890</p>
-        <p>📧 contacto@solarauto.com</p>
-      </div>
-      <div class="footer-section location">
-        <h3>Ubicación</h3>
-        <p>🚗 Av. Automotriz #123, PEREIRA</p>
-      </div>
-      <div class="footer-section social-icons">
-        <h3>Síguenos</h3>
-        <img src="../img/facebook.png" alt="Facebook">
-        <img src="../img/instagram.gif" alt="Instagram">
-        <img src="../img/twitter.png" alt="Twitter">
-      </div>
-    </div>
-
-  </footer>
+   <?php include 'include/login-footer.php'; ?>
     
 </body>
 </html>
