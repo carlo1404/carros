3980bbd8c0d945d3c464067ff369d4f1aa7b704a
Jose Obregon <joseobregon270@gmail.com>
2025-04-14 17:22:49 -0500

    Seccion footer
    Se borro la seccion de l footer en el css mio. y se agrego un link para el boton de comprar.

--- php/detalles_producto.php
+++ php/detalles_producto.php
@@ -6,25 +6,10 @@
     <title>Detalle de Vehículo</title>
     <link rel="stylesheet" href="../css/Normalize.css">
     <link rel="stylesheet" href="../css/detalles_producto.css">
+    <link rel="import" href="php/inclde/footer.php">
+    <link rel="import" href="php/inclde/header.php">
 </head>
 <body>
-    <header class="header">
-        <div class="container header__container">
-            <div class="header__menu-icon">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <div class="header__logo-group">
-                <h1 class="header__title">NOMBRE__CONCESIONARIO</h1>
-                <img src="../img/car-sports-svgrepo-com.svg" alt="Icono de auto" class="header__car-image">
-            </div>
-            
-            <div class="header__buttons">
-                <a href="../index.php"><img src="../img/icono-home.png"></a>
-            </div>
-        </div>
-    </header>
     
     <main class="container">
         <div class="product">
@@ -61,37 +46,12 @@
                     <textarea class="product__info-textarea" readonly>Vehículo en excelentes condiciones, único dueño, mantenimiento al día, aire acondicionado, sistema de navegación, cámara de reversa y más características.</textarea>
                 </div>
                 <div class="product__actions">
-                    <button class="product__button product__button--primary">COMPRAR</button>
+                    <a href="#Pagina murillo"><button class="product__button product__button--primary">COMPRAR</button></a>
                     <button class="product__button product__button--secondary">CARRITO</button>
                 </div>
             </div>
         </div>
     </main>
-    
-    <footer class="footer">
-        <div class="container footer__container">
-            <div class="footer__section">
-                <div class="footer__icon">
-                    <img src="../img/steering-wheel-svgrepo-com.svg" alt="Icono de volante" class="footer__icon-image">
-                </div>
-                <div class="footer__text">TEST DRIVE</div>
-            </div>
-
-            <div class="footer__section">
-                <div class="footer__logo">
-                    <div class="footer__text">NOMBRE__CONCESIONARIO</div>
-                </div>
-                <div class="footer__car-icon">
-                    <img src="../img/car-sports-svgrepo-com.svg" alt="Icono de auto" class="footer__icon-image">
-                </div>
-            </div>
-
-            <div class="footer__section">
-                <div class="footer__icon">
-                    <img src="../img/phone-svgrepo-com.svg" alt="Icono de teléfono" class="footer__icon-image">
-                </div>
-                <div class="footer__text">CONTACTANOS</div>
-            </div>
         </div>
     </footer>
 </body>
